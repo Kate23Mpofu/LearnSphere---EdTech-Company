fd2a008d411d534d7b7bc137edb5926a37f293dc
maMpofu <136378456+Kate23Mpofu@users.noreply.github.com>
2026-06-23 02:30:10 +0200

    Update learnsphere_analysis.sql

--- learnsphere_analysis.sql
+++ learnsphere_analysis.sql
@@ -1,3 +1,31 @@
 CREATE DATABASE LearnSphere;
 
 USE LearnSphere;
+
+-- confirm all imported tables are populated
+SELECT *
+FROM students;
+
+SELECT *
+FROM reviews;
+
+SELECT *
+FROM enrollments;
+
+SELECT *
+FROM instructors;
+
+SELECT *
+FROM lookup_table;
+
+SELECT *
+FROM courses;
+
+-- analysis
+--  Which course categories generate the most revenue, and which have the highest dropout rate?
+--  What is the course completion rate overall, and which courses/instructors underperform?
+--  Which signup source and device type bring in the most paying, completing students?
+--  Which cities/provinces should LearnSphere target for marketing spend, based on enrollment and revenue?
+-- What is the average student rating per course/instructor, and does it correlate with completion rate?
+-- What % of enrollments used a discount (amount paid below course price), and how does that affect completion?
+--  Who are the top 10 instructors by revenue generated and by average rating?
